42cdfc11185236a98a2567bab201c10bbb7acd4f
2624789 <2624789@users.noreply.github.com>
2024-04-06 13:01:40 -0500

    Use double quotes

--- app/auth/security.py
+++ app/auth/security.py
@@ -23,13 +23,13 @@ def create_access_token(
 
 
 def hash_password(plain_password: str) -> str:
-    pwd_bytes = plain_password.encode('utf-8')
+    pwd_bytes = plain_password.encode("utf-8")
     salt = bcrypt.gensalt()
     hashed_password = bcrypt.hashpw(password=pwd_bytes, salt=salt)
     return hashed_password.decode("utf-8")
 
 
 def verify_password(plain_password: str, hashed_password: str) -> bool:
-    pwd_bytes = plain_password.encode('utf-8')
-    hash_bytes = hashed_password.encode('utf-8')
+    pwd_bytes = plain_password.encode("utf-8")
+    hash_bytes = hashed_password.encode("utf-8")
     return bcrypt.checkpw(password=pwd_bytes, hashed_password=hash_bytes)
