0bb885cdcf9cae3f4e2bb10d9aadada2f94142e3
2624789 <2624789@users.noreply.github.com>
2024-04-06 13:43:11 -0500

    Adds cors setting (#2)

--- app/main.py
+++ app/main.py
@@ -1,9 +1,19 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from app.auth.router import router as auth
+from app.config import settings
 
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=settings.cors_origins,
+    allow_credentials=True,
+    allow_methods=['GET', 'POST',],
+    allow_headers=['Authorization'],
+)
 
 app.include_router(auth)
 
